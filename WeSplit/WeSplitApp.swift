9eaddd22bcb57df3af4b96a727065f3d73c92dbb
Tung Huynh <htqtung@gmail.com>
2023-04-27 14:29:08 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tung Huynh on 27.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
